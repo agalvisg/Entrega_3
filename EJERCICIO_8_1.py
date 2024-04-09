a2aaf223229854568c2ad0262d7441adc6ae48eb
agalvisg <agalvgut@myuax.com>
2024-04-09 08:23:25 +0200

    commit final del apartado 1 del ejercicio 8

--- EJERCICIO_8_1.py
+++ EJERCICIO_8_1.py
@@ -1,4 +1,4 @@
-#EJERCICIO 8
+#EJERCICIO 8 apartado 1.
 
 def precio_sin_impuestos():
     precio=float(input("Ingrese el precio del producto (euros): "))
@@ -24,28 +24,35 @@ class Impuestos(TipoProducto):
             impuestos=precio_sin_impuestos*(self.IVA/100)
             return impuestos
         else:
-            impuestos=[precio_sin_impuestos*(self.especial/100)+precio_sin_impuestos*(self.IVA/100)]
+            impuestos=precio_sin_impuestos*(self.especial/100)+precio_sin_impuestos*(self.IVA/100)
             return impuestos   
-    
+def PrecioFinal(precio_sin_impuestos,impuestos_calculados):
+    precio_final=precio_sin_impuestos+impuestos_calculados
+    return precio_final
 
 def main():
-    tipo=input("¿De qué tipo de producto desea calcular el precio?(ordinario, alcohol, hidrocarburos, tabaco)")
+    
+    tipo=input("¿De qué tipo de producto desea calcular el precio?(ordinario, alcohol, hidrocarburos, tabaco) ")
     precio=precio_sin_impuestos()
     tipo_producto=TipoProducto(tipo)
+    
     
     if tipo in ["ordinario"]:
         IVA=21
         imp=Impuestos(tipo,IVA,0)
-    elif tipo==["alcohol","hidrocarburos","tabaco"]:
+    elif tipo in ["alcohol","hidrocarburos","tabaco"]:
         IVA=21
-        especial=float(input("Ingrese el porcentaje de IVA correspodiente al producto:"))
+        especial=float(input("Ingrese el porcentaje de impuestos especiales correspodiente al producto:"))
         imp=Impuestos(tipo,IVA,especial)
     else:
         print("tipo de producto no válido.")
-        return
+        return tipo
     impuestos_calculados=imp.calcular_impuestos(precio)
-    print(f"Los impuestos para el producto {tipo} son : {impuestos_calculados} €")
+    precio_final=PrecioFinal(precio,impuestos_calculados)
     
+    
+    print(f"Los impuestos para el producto {tipo} son : {impuestos_calculados} €")
+    print(f"el precio final a pagar del producto será de {precio_final} € con IVA incluido e impuestos especiales.")
     
 if  __name__=="__main__":
     main()
